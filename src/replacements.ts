0e437d1ae1775b38ccfc656644abb2a7ce8c91e4
Артур Шарапов <105589311+qaip@users.noreply.github.com>
2023-04-26 02:17:17 +0600

    Add optional-block and semicolon constructions for templates (#28)

--- src/replacements.ts
+++ src/replacements.ts
@@ -11,12 +11,11 @@ export const getReplacements = (config: Config | Subconfig): Replacements => {
         '#RU#': config.ru,
         '#EN#': config.en,
         '#PARENT#': config.parent,
-        '#CHILDREN#': list('subject_domain_of_', config.children),
-        '#SECTION_CHILDREN#': list('section_subject_domain_of_', config.children),
-        '#MAX#': list('concept_', config.max),
-        '#CONCEPTS#': list('concept_', config.concepts),
-        '#RRELS#': list('rrel_', config.rrels),
-        '#NRELS#': list('nrel_', config.nrels),
+        '#CHILDREN#': list(config.children),
+        '#MAX#': list(config.max),
+        '#CONCEPTS#': list(config.concepts),
+        '#NRELS#': list(config.nrels),
+        '#RRELS#': list(config.rrels),
         '#ATOMIC#': config.children ? 'non_atomic' : 'atomic'
       }
     }
@@ -27,38 +26,35 @@ export const getReplacements = (config: Config | Subconfig): Replacements => {
         '#SYSTEM#': config.system,
         '#RU#': toArray(config.ru)[0],
         '#EN#': toArray(config.en)[0],
-        '#RU_ALT#': list('', toArray(config.ru).slice(1).join('\n')),
-        '#EN_ALT#': list('', toArray(config.en).slice(1).join('\n'), true),
-        '#DEFINITION_RU#': list('', toArray(config.definition.ru).join('\n')),
-        '#DEFINITION_EN#': list('', toArray(config.definition.en).join('\n')),
-        '#DEFINITION_CONCEPTS#': list('concept_', config.definition.using.concepts),
-        '#DEFINITION_NRELS#': list('nrels_', config.definition.using.nrels),
-        '#DEFINITION_RRELS#': list('rrels_', config.definition.using.rrels),
+        '#RU_ALT#': list(toArray(config.ru).slice(1).join('\n')),
+        '#EN_ALT#': list(toArray(config.en).slice(1).join('\n')),
+        '#DEFINITION_RU#': list(toArray(config.definition.ru).join('\n')),
+        '#DEFINITION_EN#': list(toArray(config.definition.en).join('\n')),
+        '#DEFINITION_CONCEPTS#': list(config.definition.using.concepts),
+        '#DEFINITION_NRELS#': list(config.definition.using.nrels),
+        '#DEFINITION_RRELS#': list(config.definition.using.rrels),
         '#STATEMENT#': template =>
           config.statement
             ? statements
                 .map(([system, variables]) => replace(template, { configType: 'statement', system, ...variables }))
                 .join('\n')
             : '',
-        '#STATEMENT_CONCEPTS_ALL#': list(
-          'concept_',
-          statements.map(statement => statement[1].using.concepts).join('\n')
-        ),
-        '#STATEMENT_NRELS_ALL#': list('nrels_', statements.map(statement => statement[1].using.nrels).join('\n')),
-        '#STATEMENT_RRELS_ALL#': list('rrels_', statements.map(statement => statement[1].using.rrels).join('\n'))
+        '#STATEMENT_CONCEPTS_ALL#': list(statements.map(statement => statement[1].using.concepts).join('\n')),
+        '#STATEMENT_NRELS_ALL#': list(statements.map(statement => statement[1].using.nrels).join('\n')),
+        '#STATEMENT_RRELS_ALL#': list(statements.map(statement => statement[1].using.rrels).join('\n'))
       }
-      return config.configType === 'concept' ? { ...nbhd, '#PARENT#': config.parent ?? '...' } : { ...nbhd }
+      return config.configType === 'concept' ? { ...nbhd, '#PARENT#': list(config.parent) } : { ...nbhd }
     }
     case 'statement':
       return {
         '#STATEMENT_SYSTEM#': config.system,
-        '#STATEMENT_RU#': list('', toArray(config.ru).join('\n')),
-        '#STATEMENT_EN#': list('', toArray(config.en).join('\n')),
+        '#STATEMENT_RU#': list(toArray(config.ru).join('\n')),
+        '#STATEMENT_EN#': list(toArray(config.en).join('\n')),
         '#STATEMENT_TITLE_RU#': config.title.ru,
         '#STATEMENT_TITLE_EN#': config.title.en,
-        '#STATEMENT_CONCEPTS#': list('concept_', config.using.concepts),
-        '#STATEMENT_NRELS#': list('nrels_', config.using.nrels),
-        '#STATEMENT_RRELS#': list('rrels_', config.using.rrels)
+        '#STATEMENT_CONCEPTS#': list(config.using.concepts),
+        '#STATEMENT_NRELS#': list(config.using.nrels),
+        '#STATEMENT_RRELS#': list(config.using.rrels)
       }
   }
 }
